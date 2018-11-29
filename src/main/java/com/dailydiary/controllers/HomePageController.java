9b5f22e51ddbaf914a77e3bc37119146d2cb0cff
Adrian F <iiadixii@o2.pl>
2018-11-29 13:24:23 +0100

    adding posts at homepage if logged, working on logout

--- src/main/java/com/dailydiary/controllers/HomePageController.java
+++ src/main/java/com/dailydiary/controllers/HomePageController.java
@@ -1,8 +1,9 @@
 package com.dailydiary.controllers;
 
-import com.dailydiary.dto.UserDTO;
+import com.dailydiary.entity.Category;
 import com.dailydiary.entity.Logs;
 import com.dailydiary.entity.User;
+import com.dailydiary.repositories.CategoryRepository;
 import com.dailydiary.repositories.LogsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
+
+import static com.dailydiary.controllers.LoginController.LOGGED_USER_KEY;
 
 @Controller
 @RequestMapping(value = "/dd", produces = "text/html;charset=UTF-8")
@@ -21,18 +26,33 @@ public class HomePageController {
     @Autowired
     LogsRepository logsRepository;
 
-    @GetMapping
-        public String prepareHomePage(Model model, @SessionAttribute(value = LoginController.LOGGED_USER_KEY, required = false) User loggedUser){
+    @Autowired
+    CategoryRepository categoryRepository;
 
-            if(loggedUser != null){
-                model.addAttribute("loggedUser",loggedUser);
-                List<Logs> userLogs = logsRepository.findAllByUserId(loggedUser.getId());
-                model.addAttribute("userLogs", userLogs);
-            }
-            List<Logs> logs = logsRepository.findAll();
+    @RequestMapping
+    public String prepareHomePage(Model model, @SessionAttribute(value = LOGGED_USER_KEY, required = false) User loggedUser, HttpServletRequest request, HttpSession session) {
+//        if (request.getParameter("logout") != null) {
+//            session.setAttribute(LOGGED_USER_KEY, null);
+//        }
+        if (loggedUser != null) {
+            model.addAttribute("newLog", new Logs());
+            model.addAttribute("loggedUser", loggedUser);
+            List<Logs> userLogs = logsRepository.findAllByUserId(loggedUser.getId());
+            model.addAttribute("userLogs", userLogs);
+
+        }
+            List<Logs> logs = logsRepository.findAllByOrderByCreatedDesc();
             model.addAttribute("allLogs", logs);
 
             return "homepage";
         }
+    @ModelAttribute("category")
+    public List<Category> categories() {
+        return categoryRepository.findAll();
+
+        }
+
+
 
     }
+
